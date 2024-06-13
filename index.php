2ed27212ba2237ea5340487e93d2c9f7825f0f72
cwmart <128403336+cwmart@users.noreply.github.com>
2024-06-13 14:40:56 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>index.php</title>
+  <link rel="stylesheet" href="./stile.css" /> 
+</head>
+
+<body>
+
+  <h1>index.php</h1>
+
+  <form action="">
+
+    <input type="text" name="login">
+    <input type="password" name="pass">
+    <input type="email" name="email">
+    <input type="tel" name="tel">
+
+    <input type="submit" value="Send data">
+
+  </form>
+
+  <a href="./index.html">index.html</a>
+</body>
+
+</html>
